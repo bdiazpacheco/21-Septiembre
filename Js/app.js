517f9b6010754c8786558f9404e8ecf5ce76f015
bdiazpacheco <brialdipa1494@gmail.com>
2024-09-21 21:45:08 -0500

    Version 2.0

--- Js/app.js
+++ Js/app.js
@@ -2,7 +2,7 @@ let Titulo = document.title;
 
 window.addEventListener('blur', () => {
     Titulo = document.title;
-    document.title = "No te vallas, regresa :(";
+    document.title = "No te vallas, regresa cuchifluuuu";
 })
 
 window.addEventListener('focus', () => {
